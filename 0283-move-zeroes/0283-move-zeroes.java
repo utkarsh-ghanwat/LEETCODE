8c509b0ee7de208049380d29376170a563257f61
utkarsh-ghanwat <utkarshghanwat45@gmail.com>
2026-09-02 23:47:04 +0530

    Time: 3 ms (17.89%), Space: 47.7 MB (57.59%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,6 +1,8 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+
         int left = 0;
+        
         for(int right = 0; right < nums.length; right++){
             if (nums[right] != 0){
                 int temp = nums[right];
